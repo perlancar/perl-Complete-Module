74536c5ef0345466ac4bbacc3c9520e7da18d027
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-26 12:15:27 +0700

    Load B module during testing before emptying @INC, as it's required by new Test::Simple (1.301002+) [CT]

--- t/testlib.pl
+++ t/testlib.pl
@@ -5,13 +5,16 @@ use warnings;
 use Complete;
 use Complete::Module qw(complete_module);
 use Complete::Path;
-use Data::Dumper; # force use, before we empty @INC
 use File::Slurp::Tiny qw(write_file);
 use File::Temp qw(tempdir);
 use Filesys::Cap qw(fs_is_cs);
 use Test::More 0.98;
-use mro; # force use, before we empty @INC
 use Test::More 0.98;
+
+# force load these first as these are required during test, before we empty @INC
+use B; # new Test::Simple 1.301+ requires this
+use Data::Dumper;
+use mro;
 
 sub test_complete {
     my %args = @_;
